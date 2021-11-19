93f4d86c493d3ef13b8a613a0f4eeb60d4785c95
akansha baishwade <akanshabaishwade@gmial.com>
2021-11-19 15:58:53 +0530

    Initial commit

---
+++ pyton_basic(chapter 1)/Math_Operators.py
@@ -0,0 +1,48 @@
+# exponent
+exponent = 10 ** 10
+print(exponent, "exponent")
+
+Modulus = 10 % 10
+print(Modulus, "Modulus")
+
+integer_division  = 10 // 10
+print(integer_division, "integer_division")
+
+Division = 10 / 10
+print(Division, "Division")
+
+Multiply= 10 * 10
+print(Multiply, "Multiply")
+
+subtract = 10 - 10
+print(subtract, "subtract")
+
+Addition = 10 + 10
+print(Addition, "Addition")
+
+
+# Extra
+
+# 'Alice' + 42  =  #wrong
+# 1 + 1 = right
+# 'str' + 'str' = right
+# 'Alice' * 5 = right
+print('Alice' * 5)
+print('Alice\n' * 5) #for newline
+
+
+
+# output
+# 10000000000 exponent
+# 0 Modulus
+# 1 integer_division
+# 1.0 Division
+# 100 Multiply
+# 0 subtract
+# 20 Addition
+# AliceAliceAliceAliceAlice
+# Alice
+# Alice
+# Alice
+# Alice
+# Alice
---
+++ pyton_basic(chapter 1)/print.py
@@ -0,0 +1,25 @@
+# direct Print
+print("hello word")
+print(3)
+print(4.5)
+
+# print with variable
+a = 4
+b = "variable"
+c = 3.4
+print(a)
+print(b)
+print(c)
+print(a, b, c, "all")
+
+# print with function
+def Print():
+    a = 3
+    b = 4
+    c = a + b
+    print(c, "am in function")
+
+Print()
+
+# print with class
+
